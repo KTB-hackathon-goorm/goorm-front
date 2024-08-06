33c0c505421ddaafad4a48a98d45b1fcb86fd6f0
mia (Yuna) <yuna00708@gmail.com>
2024-08-06 14:05:56 +0900

    feat: 로그인 및 라우팅 설정

---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('/login', { email, password });
+            if (response.status === 200) {
+                navigate('/question');
+            } else {
+                console.error('로그인 실패!!!', response.data);
+                alert("로그인에 실패하였습니다. 다시 시도해주세요.");
+            }
+        } catch (error) {
+            console.error('로그인 중 에러 발생: ', error);
+        }
+    }
+
+    return (
+        <>
+            <div className="header">
+                <h1>BONGU</h1>
+            </div>
+            <div className="container">
+                <div className="input-container">
+                    <form onSubmit={handleSubmit}>
+                        <h2>로그인</h2>
+                        <input
+                            type="email"
+                            placeholder="이메일"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="비밀번호"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            type='submit'
+                        >로그인</button>
+                    </form>
+                </div>
+            </div>
+        
+        </>
+    )
+}
+
+export default LoginPage;
