a7ead26ce6a8c58726452e7c50d098a53fbfa6a0
mia (Yuna) <yuna00708@gmail.com>
2024-08-06 19:24:58 +0900

    feat: 메인페이지 레이아웃

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
 import LoginPage from './pages/Login/LoginPage';
 import QuestionPage from './pages/Question/QuestionPage';
-//import Main from './pages/Main';
+import MainPage from './pages/Main/MainPage';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/question" element={<QuestionPage />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
     </Router>
 
