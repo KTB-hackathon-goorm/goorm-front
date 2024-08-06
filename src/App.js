bda1786f1da239ff4fdc2cfaf8a3268d26c458c1
mia (Yuna) <yuna00708@gmail.com>
2024-08-06 20:35:43 +0900

    feat: info 화면

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header'
 import LoginPage from './pages/Login/LoginPage';
 import QuestionPage from './pages/Question/QuestionPage';
 import MainPage from './pages/Main/MainPage';
+import InfoPage from './pages/Info/InfoPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/question" element={<QuestionPage />} />
         <Route path="/main" element={<MainPage />} />
+        <Route path="/info" element={<InfoPage />} />
       </Routes>
     </Router>
 
