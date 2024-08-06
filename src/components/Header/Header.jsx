af554dd40fade65b806b3d97a7b8efdbf48285e1
mia (Yuna) <yuna00708@gmail.com>
2024-08-07 01:40:07 +0900

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,14 @@ function Header() {
     return (
         <>
             <div className="header">
-                <h1>BONGU</h1>
+                <h1>BONGGU</h1>
             </div>
             <div className="divider-container">
                 <div className="divider"></div>
             </div>
         </>
     );
+
 }
 
 export default Header;
